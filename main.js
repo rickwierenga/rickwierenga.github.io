bd96c45f2ec4184d86af9e4a383c3c6b13dd546c
rickwierenga <rick_wierenga@icloud.com>
2019-04-30 13:42:24 +0200

    new site

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-/* cards */
-$('.flip').click(function(){
-    if ($(this).find('.card').hasClass('flipped')) {
-        $(this).find('.card').removeClass('flipped');
-    } else {
-        $(this).find('.card').addClass('flipped');
-    }
-    return true;
-});
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-/* smooth scrolling */
-$(document).ready(function(){
-    /* source: w3schools 
-    * https://www.w3schools.com/howto/howto_css_smooth_scroll.asp 
-    */
 
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, {
-                duration: 1000, 
-                easing: 'linear'
-            }, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 500, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
