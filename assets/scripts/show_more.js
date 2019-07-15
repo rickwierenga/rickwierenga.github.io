e9390de3d3ce2d13cec238d0477594fe09264629
rickwierenga <rick_wierenga@icloud.com>
2019-07-15 23:01:14 +0200

    fix front page contact info mobile layout

--- assets/scripts/show_more.js
+++ assets/scripts/show_more.js
@@ -5,16 +5,22 @@ var btn_label = document.getElementsByClassName('btn-label')[0];
 
 function toggleSocialMedia() {
 	if (shown) {
-		social_media.style.display = "none";
+		social_media.classList.remove("hidden");
 		toggle_icon.classList.remove("fa-chevron-up");
 		toggle_icon.classList.add("fa-chevron-down");
 		btn_label.innerHTML = 'View ';
 		shown = false;
 	} else {
-		social_media.style.display = "initial";
+		social_media.classList.add("hidden");
 		toggle_icon.classList.remove("fa-chevron-down");
 		toggle_icon.classList.add("fa-chevron-up");
 		btn_label.innerHTML = 'Hide ';
 		shown = true;
 	}
+}
+
+window.onload = function(e){ 
+	if (window.screen.availWidth < 667) {
+	    social_media.classList.add("hidden");
+    }
 }
